799d76f1b601de761d405e7ca2dbdb93ccb5b02f
lizianecastegnaro <97547186+lizianecastegnaro@users.noreply.github.com>
2026-02-05 15:49:42 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'inspecao-eolica-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+// Instalação do Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Ativação - limpa caches antigos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Removendo cache antigo:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Intercepta requisições - estratégia Cache First
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Retorna do cache se existir
+        if (response) {
+          return response;
+        }
+        // Senão, busca da rede
+        return fetch(event.request);
+      }
+    )
+  );
+});
